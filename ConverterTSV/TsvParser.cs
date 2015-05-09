d65e472ca2467debbc21de1de9e5c3bffce1ef1f
MireGitHub <mirella.varchetta@gmail.com>
2015-05-09 22:43:03 +0200

    TsvParser implemented

--- ConverterTSV/TsvParser.cs
+++ ConverterTSV/TsvParser.cs
@@ -1,14 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ConverterTSV
 {
     public class TsvParser
     {
         public List<string> ValuesToKeep { get; set; }
+        private const char Separator = '\t';
 
         public FileTsv ParseTsvFile(string filePath)
         {
-            throw new System.NotImplementedException();
+            var file = new FileTsv();
+            string[] values = new string[] { };
+
+            try
+            {
+                var reader = new StreamReader(filePath);
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        continue;
+                    }
+
+                    values = line.Split(Separator);
+
+                    if (ValuesToKeep.Contains(values[0]))
+                    {
+                        file.Add(values[0], int.Parse(values[1]));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                throw new IOException(e.Message);
+            }
+            catch (FormatException e)
+            {
+                throw new FormatException(String.Format("Cannot parse value '{0}' for label '{1}'", values[1], values[0]));
+            }
+
+            return file;
         }
     }
 }
