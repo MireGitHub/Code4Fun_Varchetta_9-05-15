b65197e227be8ef7ec81e8512d3a8f378666d142
MireGitHub <mirella.varchetta@gmail.com>
2015-05-09 19:38:42 +0200

    Add TsvCollection and test

---
+++ ConverterTSV.Test/FileTsvCollectionTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace ConverterTSV.Test
+{
+    [TestFixture]
+    public class FileTsvCollectionTest
+    {
+        private FileTsvCollection _collection;
+        private int _resultSum;
+        private double _resultAverage;
+
+        [SetUp]
+        public void Setup()
+        {
+            _collection = new FileTsvCollection();
+        }
+
+        [Test]
+        public void Average_of_two_values()
+        {
+            _collection.Clear();
+            _collection.Add(new FileTsv { { "latency_ms", 1 } });
+            _collection.Add(new FileTsv { { "latency_ms", 2 } });
+            _resultAverage = _collection.Average("latency_ms");
+
+            Assert.That(_resultAverage, Is.Positive);
+            Assert.That(_resultAverage, Is.EqualTo(1.5));
+        }
+
+        [Test]
+        public void Sum_of_two_values()
+        {
+            _collection.Clear();
+            _collection.Add(new FileTsv { { "bandwidth", 1 } });
+            _collection.Add(new FileTsv { { "bandwidth", 2 } });
+            _resultSum = _collection.Sum("bandwidth");
+
+            Assert.That(_resultSum, Is.Positive);
+            Assert.That(_resultSum, Is.EqualTo(3));
+        }
+    }
+}
---
+++ ConverterTSV/FileTsvCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ConverterTSV
+{
+    public class FileTsvCollection : Collection<FileTsv>
+    {
+        public double Average(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Sum(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
